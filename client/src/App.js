71ca30e95f5addf5a13cde4e5fae106b550cce86
tomytank <github@iamtom.com>
2020-05-27 10:54:12 +0200

    MovieList Route added, props passed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -15,6 +17,7 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
+          // console.log(movieList);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -26,12 +29,22 @@ const App = () => {
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
-
+// console.log(movieList);
   return (
+    <Router>
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route exact path="/" >
+          <MovieList key={movieList.id} movies={movieList}/>
+        </Route>
+        <Route path="/movies/:id">
+          
+        </Route>
+      </Switch>
+      {/* <div>Replace this Div with your Routes</div> */}
     </div>
+    </Router>
   );
 };
 
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const MovieList = props => {
+  console.log("Props (MovieList)",props);
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
